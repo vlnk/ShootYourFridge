a0c9f11ec4712b194b938dfef908417225c524b1
rvermes <annesohier@MacBook-Pro-de-Anne-SOHIER.local>
2015-04-25 23:23:34 +0200

    avant garde de la recherche de recette

--- SYF/src/com/project/SYF/RechercheRecette.java
+++ SYF/src/com/project/SYF/RechercheRecette.java
@@ -1,9 +1,11 @@
 package com.project.SYF;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,16 +13,26 @@ import java.util.HashMap;
 import android.content.Intent;
 import org.jsoup.nodes.Document;
 
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
 /**
  * Created by robinvermes on 25/04/2015.
  */
-public class RechercheRecette extends Activity{
+public class RechercheRecette extends ListActivity {
 
     private ArrayList<String> mAlimentList = new ArrayList<String>();
     private ArrayAdapter<String> mAlimentListAdapter;
-    private ListView mAlimentListView;
+    private ListView mRecetteListView;
 
     private ArrayList<HashMap<String, String>> recetteList;
+
+    private static final String TAG_NAME = "name";
+    private static final String TAG_DETAILS = "details";
+    private static final String TAG_DESCRIPTION = "description";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,22 +43,25 @@ public class RechercheRecette extends Activity{
         Intent intent = getIntent();
         mAlimentList = intent.getStringArrayListExtra("ingredients");
 
-        //list Aliment
-        mAlimentListView = (ListView) findViewById(R.id.list_lien_recette);
-        mAlimentListAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                mAlimentList);
-        mAlimentListView.setAdapter(mAlimentListAdapter);
-        mAlimentListView.setOnItemClickListener(new ListView.OnItemClickListener() {
+        recetteList = new ArrayList<HashMap<String, String>>();
+
+      //  mRecetteListView = (ListView) findViewById(R.id.list_lien_recette);
+        mRecetteListView = getListView();
+
+
+        mRecetteListView.setOnItemClickListener(new ListView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //recherche internet de la recette
+                //traitement : lancement de la recette sur internet
             }
         });
+
+
 
         AsyncRechercheRecettes mTask = new AsyncRechercheRecettes(this, mAlimentList);
         mTask.execute();
 
     }
+
 
     public void finalizeResearch(Document document){
 
@@ -55,11 +70,12 @@ public class RechercheRecette extends Activity{
 
 
         //actualiser la liste
-        mAlimentList.clear();
+        ListAdapter adapter = new SimpleAdapter(
+                RechercheRecette.this, recetteList,
+                R.layout.recette, new String[] { TAG_NAME, TAG_DETAILS,
+                TAG_DESCRIPTION }, new int[] { R.id.name,
+                R.id.details, R.id.description });
 
-
-
-
-        mAlimentListAdapter.notifyDataSetChanged();
+        setListAdapter(adapter);
     }
 }
