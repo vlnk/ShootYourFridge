84107d988fedfab7b88429736c7f86acd1edcb30
vlnk <dftvlnk@gmail.com>
2015-04-26 19:13:30 -0400

    Cleanup code

--- SYF/src/com/project/SYF/dialogs/NoEntryFoundAlertDialog.java
+++ SYF/src/com/project/SYF/dialogs/NoEntryFoundAlertDialog.java
@@ -1,18 +1,14 @@
 package com.project.SYF.dialogs;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import com.project.SYF.R;
 
-/**
- * Created by mimmy on 26/04/15.
- */
 public class NoEntryFoundAlertDialog extends DialogFragment {
 
+    @SuppressWarnings("Convert2Lambda")
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
