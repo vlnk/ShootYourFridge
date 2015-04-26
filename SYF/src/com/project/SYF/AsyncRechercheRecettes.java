d5401a9c4f9eb5e47e360047d0d4ee936f6cb4ec
vlnk <dftvlnk@gmail.com>
2015-04-26 19:12:43 -0400

    Cleanup code

--- SYF/src/com/project/SYF/AsyncRechercheRecettes.java
+++ SYF/src/com/project/SYF/AsyncRechercheRecettes.java
@@ -3,33 +3,14 @@ package com.project.SYF;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
-/**
- * Created by annesohier on 25/04/2015.
- */
 
 public class AsyncRechercheRecettes extends AsyncTask<Void, Integer, Boolean> {
 
@@ -37,15 +18,15 @@ public class AsyncRechercheRecettes extends AsyncTask<Void, Integer, Boolean> {
     private static final String url1 = "http://www.marmiton.org/recettes/recherche.aspx?aqt=";
     private static final String url2 = "";
 
-    private String mUrlString;
+    private final String mUrlString;
 
     private Document document;
 
-    private ArrayList<String> mAlimentList = new ArrayList<String>();
+    private ArrayList<String> mAlimentList = new ArrayList<>();
 
     public AsyncRechercheRecettes(Activity rechercheActivity, ArrayList<String> alimentList) {
         super();
-        this.mActivity = new WeakReference<RechercheRecette>((RechercheRecette) rechercheActivity);
+        this.mActivity = new WeakReference<>((RechercheRecette) rechercheActivity);
         this.mAlimentList = alimentList;
         mUrlString = url1 + createURL() + url2;
     }
@@ -70,7 +51,7 @@ public class AsyncRechercheRecettes extends AsyncTask<Void, Integer, Boolean> {
 
     @Override
     protected Boolean doInBackground(Void... params) {
-        URL urlToCheck = null;
+        URL urlToCheck;
 
         try {
             urlToCheck = new URL(mUrlString);
