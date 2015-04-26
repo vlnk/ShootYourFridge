87e8f5ea0fe09b4e0f174db3e9b555f3fe5e6b6b
Michele Biamino <michele.biamino@gmail.com>
2015-04-26 19:23:55 -0400

    Suppression éléments

--- SYF/src/com/project/SYF/model/Recipe.java
+++ SYF/src/com/project/SYF/model/Recipe.java
@@ -5,5 +5,42 @@ package com.project.SYF.model;
  */
 public class Recipe {
 
+    int     id;
+    String  name;
+    String  details;
+    String  description;
+    String  href;
+
+    public Recipe() {}
+
+    public Recipe(int id, String name, String details, String description,
+                  String href){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.href = href;
+        this.details = details;
+    }
+
+
+    // getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDetails() { return details; }
+    public String getDescription() { return description; }
+    public String getHref() { return href; }
+
+
+    // setters
+    public void setHref(String href) { this.href = href; }
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name;  }
+    public void setDetails(String details) { this.details = details; }
+
+    public void setDescription(String description) {
+        this.description =  description;
+    }
+
+
 
 }
